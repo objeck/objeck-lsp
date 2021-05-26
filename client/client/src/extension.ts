7883dba670a6b91eece682842d18f35d243a806d
Randy Hollines <objeck@gmail.com>
2021-05-26 13:17:26 -0700

    wip

--- client/client/src/extension.ts
+++ client/client/src/extension.ts
@@ -25,12 +25,12 @@ export function activate(context: ExtensionContext) {
     };
 
 	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
+	// let serverModule = context.asAbsolutePath(
+	//	path.join('server', 'out', 'server.js')
+	// );
 	// The debug options for the server
 	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	// let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
@@ -58,7 +58,7 @@ export function activate(context: ExtensionContext) {
 		// Register the server for plain text documents
 		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
 		synchronize: {
-            fileEvents: workspace.createFileSystemWatcher('**/*.*')
+            fileEvents: workspace.createFileSystemWatcher('**/*.clientrc')
         }
 	};
 
