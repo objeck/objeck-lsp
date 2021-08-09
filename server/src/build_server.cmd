22dfaff418805642c49f2d2dd035958a9b62b70e
Randy Hollines <objeck@gmail.com>
2021-08-08 17:06:26 -0700

    adding diagnostics library to build process

--- server/src/build_server.cmd
+++ server/src/build_server.cmd
@@ -1,1 +1,5 @@
+@echo off
+
+SET PATH=%PATH%;..\..\..\objeck-lang\core\release\deploy64\bin
+SET OBJECK_LIB_PATH=..\..\..\objeck-lang\core\release\deploy64\lib
 obc -src *.obs -lib diags,net,json,regex -dest objk_lang_server.obe
