cd353a2bbd01f166c41a6f23f79a8965df6965c8
Randy Hollines <objeck@gmail.com>
2021-08-12 02:21:43 -0700

    testing lsp on linux

--- server/src/build_server.sh
+++ server/src/build_server.sh
@@ -1,1 +1,5 @@
-obc -src *.obs -lib net,json,regex -dest objk_lang_server.obe
+export PATH=$PATH:/home/objeck/Documents/Code/objeck-lang/core/release/deploy/bin
+export OBJECK_LIB_PATH=/home/objeck/Documents/Code/objeck-lang/core/release/deploy/lib
+
+obc -src \*.obs -lib diags,net,json,regex -dest objk_lang_server.obe
+obr objk_lang_server.obe 6013
