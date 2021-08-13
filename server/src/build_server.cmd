3d274cdb02ae225000523256834c97e45a88a920
Randy Hollines <objeck@gmail.com>
2021-08-13 12:59:46 -0700

    code clean up

--- server/src/build_server.cmd
+++ server/src/build_server.cmd
@@ -1,5 +1,10 @@
 @echo off
 
-SET PATH=%PATH%;..\..\..\objeck-lang\core\release\deploy64\bin
-SET OBJECK_LIB_PATH=..\..\..\objeck-lang\core\release\deploy64\lib
+SET OBJECK_ROOT=..\..\..\objeck-lang
+
+SET PATH=%PATH%;%OBJECK_ROOT%\core\release\deploy64\bin
+SET OBJECK_LIB_PATH=%OBJECK_ROOT%\core\release\deploy64\lib
+
+obc -src %OBJECK_ROOT%\core\compiler\lib_src\diags.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_ROOT%\core\lib\diags.obl
+echo ---
 obc -src *.obs -lib diags,net,json,regex -dest objk_lang_server.obe
