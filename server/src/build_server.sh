bc8c62a155bd195ecb42a16c1a40363762046b12
Randy Hollines <objeck@gmail.com>
2021-08-21 22:03:32 -0700

    code clean up for linux

--- server/src/build_server.sh
+++ server/src/build_server.sh
@@ -1,5 +1,5 @@
-export PATH=$PATH:/home/objeck/Documents/Code/objeck-lang/core/release/deploy/bin
-export OBJECK_LIB_PATH=/home/objeck/Documents/Code/objeck-lang/core/release/deploy/lib
+export PATH=$PATH:../../../objeck-lang/core/release/deploy/bin
+export OBJECK_LIB_PATH=../../../objeck-lang/core/release/deploy/lib
 
 obc -src frameworks.obs,parser.obs,server.obs -lib diags,net,json,regex -dest objk_lang_server.obe
 obr objk_lang_server.obe 6013
