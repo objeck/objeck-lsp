2bfc61ce280c7849f18eb74aa77eabae4aa44ad9
Randy Hollines <objeck@gmail.com>
2021-09-01 07:48:34 -0700

    debugging single and multi file support

--- server/src/build_server.cmd
+++ server/src/build_server.cmd
@@ -6,12 +6,12 @@ SET OBJECK_ROOT=..\..\..\objeck-lang
 SET PATH=%PATH%;%OBJECK_ROOT%\core\release\deploy64\bin
 SET OBJECK_LIB_PATH=%OBJECK_ROOT%\core\release\deploy64\lib
 
-obc -src %OBJECK_ROOT%\core\compiler\lib_src\diags.obs -lib gen_collect -tar lib -opt s3 -dest %OBJECK_ROOT%\core\lib\diags.obl
+obc -src %OBJECK_ROOT%\core\compiler\lib_src\diags.obs -lib gen_collect -tar lib -dest %OBJECK_ROOT%\core\lib\diags.obl
 copy %OBJECK_ROOT%\core\lib\diags.obl %OBJECK_ROOT%\core\release\deploy64\lib\diags.obl
 
 echo ---
 
-obc -src *.obs -lib diags,net,json,regex -dest objk_lang_server.obe
+obc -src *.obs -lib diags,net,json,regex -dest objk_lang_server.obe -debug
 
 if [%1] NEQ [brun] goto end
 echo ---
