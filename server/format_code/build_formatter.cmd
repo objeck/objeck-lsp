a9b856626488095166b0319fc0895de832f84f5f
Randy Hollines <objeck@gmail.com>
2022-02-26 09:44:18 -0800

    adding main class

--- server/format_code/build_formatter.cmd
+++ server/format_code/build_formatter.cmd
@@ -1,6 +1,6 @@
 @echo off
 del *.obe
-obc -src formatter.obs,scanner.obs -lib gen_collect -dest code_formatter
+obc -src test.obs,formatter.obs,scanner.obs -lib gen_collect -dest code_formatter
 if "%~1" == "" goto end
 obr code_formatter %1
 :end
