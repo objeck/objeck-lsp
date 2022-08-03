54ac898c21a97d0775710ec779cb5f6abd65172d
Randy Hollines <objeck@gmail.com>
2022-08-03 13:29:40 -0700

    working on code formatting

--- server/format_code/build_formatter.cmd
+++ server/format_code/build_formatter.cmd
@@ -1,6 +1,8 @@
 @echo off
 del *.obe
 obc -src test.obs,formatter.obs,scanner.obs -lib gen_collect -dest code_formatter
+
 if "%~1" == "" goto end
-obr code_formatter %1
+	echo ---
+	obr code_formatter %1
 :end
