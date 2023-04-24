a488d0c88e48f090b76298d08c6f23b928bf1707
Randy Hollines <objeck@gmail.com>
2023-04-24 06:22:35 -0700

    build support for msys

--- server/build_server.sh
+++ server/build_server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 OBJECK_ROOT=../../objeck-lang
+DEPLOY_DIR=deploy-msys2-ucrt
 
-export PATH=$PATH:$OBJECK_ROOT/core/release/deploy/bin
-export OBJECK_LIB_PATH=$OBJECK_ROOT/core/release/deploy/lib
+export PATH=$PATH:$OBJECK_ROOT/core/release/$DEPLOY_DIR/bin
+export OBJECK_LIB_PATH=$OBJECK_ROOT/core/release/$DEPLOY_DIR/lib
 
 rm *.obe
 rm -f /tmp/objk-*
@@ -11,7 +12,7 @@ rm -f /tmp/objk-*
 echo ---
 
 obc -src $OBJECK_ROOT/core/compiler/lib_src/diags.obs -lib gen_collect -tar lib -dest $OBJECK_ROOT/core/lib/diags.obl
-cp $OBJECK_ROOT/core/lib/diags.obl $OBJECK_ROOT/core/release/deploy/lib/diags.obl
+cp $OBJECK_ROOT/core/lib/diags.obl $OBJECK_ROOT/core/release/$DEPLOY_DIR/lib/diags.obl
 
 obc -src frameworks.obs,proxy.obs,server.obs,format_code/scanner.obs,format_code/formatter.obs -lib diags,net,json,regex -dest objeck_lsp.obe
 
