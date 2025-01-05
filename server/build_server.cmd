b10c6ca850e58d40de3afa93c6a69632dac6c380
Randy Hollines <objeck@gmail.com>
2025-01-05 11:23:33 -0700

    bumping version

--- server/build_server.cmd
+++ server/build_server.cmd
@@ -4,8 +4,8 @@ SETLOCAL
 SET PORT=%1
 SET OBJECK_ROOT=..\..\objeck-lang
 
-SET PATH=%PATH%;%OBJECK_ROOT%\core\release\deploy64\bin
-SET OBJECK_LIB_PATH=%OBJECK_ROOT%\core\release\deploy64\lib
+SET PATH=%PATH%;%OBJECK_ROOT%\core\release\deploy-x64\bin
+SET OBJECK_LIB_PATH=%OBJECK_ROOT%\core\release\deploy-x64\lib
 
 del /q *.obe
 del /q %TEMP%\objk-*
@@ -13,7 +13,7 @@ del /q %TEMP%\objk-*
 echo ---
 
 obc -src %OBJECK_ROOT%\core\compiler\lib_src\diags.obs -lib gen_collect -tar lib -dest %OBJECK_ROOT%\core\lib\diags.obl
-copy %OBJECK_ROOT%\core\lib\diags.obl %OBJECK_ROOT%\core\release\deploy64\lib\diags.obl
+copy %OBJECK_ROOT%\core\lib\diags.obl %OBJECK_ROOT%\core\release\deploy-x64\lib\diags.obl
 
 echo ---
 
